2699298e34faecd4f7888c1717776a8a5def48a3
MSOB7YY <mohamedsobhy201566@gmail.com>
2025-03-10 19:04:30 +0200

    core: cross platform cipher support

--- lib/namico_db_wrapper.dart
+++ lib/namico_db_wrapper.dart
@@ -5,8 +5,8 @@ import 'dart:convert';
 import 'dart:ffi';
 import 'dart:io';
 import 'dart:isolate';
+import 'dart:math' as math;
 
-import 'package:sqlcipher_flutter_libs/sqlcipher_flutter_libs.dart' as sqllibs;
 import 'package:sqlite3/open.dart' as sqlopen;
 import 'package:sqlite3/sqlite3.dart';
 
@@ -30,10 +30,40 @@ part 'src/namico_db_wrapper_base.dart';
 typedef CacheWriteItemToEntryCallback<E> = MapEntry<String, Map<String, dynamic>?> Function(E item);
 
 class NamicoDBWrapper {
-  static initialize() {
-    DynamicLibrary? lib;
-    sqlopen.open.overrideFor(sqlopen.OperatingSystem.android, () {
-      return lib ??= sqllibs.openCipherOnAndroid();
+  static DynamicLibrary? _lib;
+
+  static void initialize() {
+    sqlopen.open.overrideForAll(() {
+      return _lib ??= _sqlcipherOpen();
     });
   }
+}
+
+DynamicLibrary _sqlcipherOpen() {
+  // Taken from https://github.com/simolus3/sqlite3.dart/blob/e66702c5bec7faec2bf71d374c008d5273ef2b3b/sqlite3/lib/src/load_library.dart#L24
+  if (Platform.isLinux || Platform.isAndroid) {
+    try {
+      return DynamicLibrary.open('libsqlcipher.so');
+    } catch (_) {
+      if (Platform.isAndroid) {
+        // On some (especially old) Android devices, we somehow can't dlopen
+        // libraries shipped with the apk. We need to find the full path of the
+        // library (/data/data/<id>/lib/libsqlite3.so) and open that one.
+        // For details, see https://github.com/simolus3/moor/issues/420
+        final appIdAsBytes = File('/proc/self/cmdline').readAsBytesSync();
+
+        // app id ends with the first \0 character in here.
+        final endOfAppId = math.max(appIdAsBytes.indexOf(0), 0);
+        final appId = String.fromCharCodes(appIdAsBytes.sublist(0, endOfAppId));
+
+        return DynamicLibrary.open('/data/data/$appId/lib/libsqlcipher.so');
+      }
+
+      rethrow;
+    }
+  }
+  if (Platform.isWindows) return DynamicLibrary.open('sqlite3.dll');
+  if (Platform.isIOS) return DynamicLibrary.process();
+  if (Platform.isMacOS) return DynamicLibrary.open('/usr/lib/libsqlite3.dylib');
+  throw UnsupportedError('Unsupported platform: ${Platform.operatingSystem}');
 }
